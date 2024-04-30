19de02ec8d0918cffc5eaed1682591f6966d195c
Gabbo-CYBER <gabry29101998@gmail.com>
2024-04-30 12:24:42 +0200

    pari e dispari end

---
+++ second.js
@@ -0,0 +1,25 @@
+function getRandomIntInclusive(min,max) {
+    min = Math.ceil (min);
+    max = Math.floor (max);
+    return Math.floor (Math.random() * (max - min + 1) + min)
+}
+
+const scelta = prompt ('Pari o Dispari?')
+const numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'))
+const numeroPC = getRandomIntInclusive(1, 5)
+const somma = numeroUtente + numeroPC
+console.log (somma)
+
+function isEven(num) {
+    return num % 2 === 0 
+}
+
+function isOdd(num) {
+    return num % 2 === 1
+}
+
+if( (scelta === 'pari' && isEven(somma)) || (scelta === 'dispari' && isOdd(somma))) {
+    console.log ('hai vinto')
+} else {
+    console.log ('hai perso')
+}
